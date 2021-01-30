8f5583815ca9708fef9c0fb90fd37ef248640fef
Mourad Chairi <33106737+Mourad-Chairi@users.noreply.github.com>
2021-01-30 22:08:38 +0100

    Add files

---
+++ IServiceEnseignement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceEnseignement
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IServiceEnseignement" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IServiceEnseignement
+    {
+        [OperationContract]
+        List<Eleve> getByCNE(string s);
+    }
+}
